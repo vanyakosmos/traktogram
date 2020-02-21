e4b34bda07f53f5b9982b657fa0efb06c46e461e
Bachynin Ivan <bachynin.i@gmail.com>
2020-02-21 23:03:12 +0200

    generate animedao slug based on MAL anime title instead of url

--- traktogram/anime.py
+++ traktogram/anime.py
@@ -12,25 +12,24 @@ generic_season_ending = re.compile(r'(1st|first|2nd|second|3rd|third|\d+th) seas
 
 
 class AnimeService:
-    def extract_mal_data(self, text: bytes):
+    def extract_mal_title(self, text: bytes):
         root = html.fromstring(text)
         row = root.xpath("//div[@id='content']/descendant::table[last()]/descendant::tr[2]")[0]
         info = row.xpath("td[2]")[0]
         link = info.xpath(".//a[starts-with(@href, 'https://myanimelist.net/anime/')]")[0]
         title = link.text_content().strip()
         title = white_space_re.sub(' ', title)
-        return title, link.get('href')
+        return title
 
-    async def get_mal_data(self, session: ClientSession, query: str):
+    async def get_mal_title(self, session: ClientSession, query: str):
         url = URL('https://myanimelist.net/anime.php')
         url = url.update_query(q=query)
         r = await session.get(url)
         text = await r.read()
-        return self.extract_mal_data(text)
+        return self.extract_mal_title(text)
 
-    def make_animedao_url(self, mal_url: str, episode: int):
-        slug = mal_url.split('/')[-1]
-        slug = slugify(slug)
+    def make_animedao_url(self, title: str, episode: int):
+        slug = slugify(title)
         return f'https://animedao.com/watch-online/{slug}-episode-{episode}'
 
 
@@ -49,15 +48,15 @@ async def get_animedao_url(show_name: str, season: int, episode: int, episode_ab
     anime = AnimeService()
     async with ClientSession(trust_env=True, **kwargs) as session:
         if season == 1:
-            title, url = await anime.get_mal_data(session, query)
-            title1, url1 = title, url
+            title = await anime.get_mal_title(session, query)
+            title1 = title
         else:
-            ((title1, url1), (title, url)) = await asyncio.gather(
-                anime.get_mal_data(session, show_name),
-                anime.get_mal_data(session, query)
+            title1, title = await asyncio.gather(
+                anime.get_mal_title(session, show_name),
+                anime.get_mal_title(session, query)
             )
-    merged_seasons = anime.make_animedao_url(url1, episode_abs)
-    split_seasons = anime.make_animedao_url(url, episode)
+    merged_seasons = anime.make_animedao_url(title1, episode_abs)
+    split_seasons = anime.make_animedao_url(title, episode)
 
     if both:
         # return url for merged and split versions
